e9163145efa0640b3889334cda1a284cc729a47e
NickDevD <nick.developerdev@gmail.com>
2024-06-02 14:33:26 -0300

    update 1.1

---
+++ tipos-variaveis/src/operadores/operadores.java
@@ -0,0 +1,31 @@
+package operadores;
+
+public class operadores {
+    public static void main(String[] args) {
+        
+    //classe Operdaores.java
+    
+    //ATRIBUIÇÃO 
+    //String nome = "Nick";
+    //int idade = 22;
+    //double peso = 68.5;
+    //char sexo = 'M';
+    //boolean doadorOrgao = false;
+    //Date dataNascimento = new Date();
+    
+    //ARITMÉTICOS
+    //double soma = 10 + 15;
+    //int subtração = 113 - 25;
+    //multiplicação = 20 * 7;
+    //int divisão = 15 / 3;
+    //int modulo = 18 % 3;
+    //double resultado = (10 * 7) + (20/4);
+
+    //CONCATENAÇÃO
+    String nomeCompleto = "LINGUAGEM " + "JAVA";
+    
+    System.out.println(nomeCompleto);
+
+    }
+
+}
