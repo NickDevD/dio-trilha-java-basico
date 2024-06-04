c6be4fe452203600a3d0a482785a29e6cc0e6218
NickDevD <nick.developerdev@gmail.com>
2024-06-04 19:40:16 -0300

    update 1.3

--- tipos-variaveis/src/metodos-parâmetros/MetodosParametros.java
+++ tipos-variaveis/src/metodos-parâmetros/MetodosParametros.java
@@ -2,8 +2,18 @@ public class metodosparametros {
     
     public static void main(String[] args) {
         
-        int numero = 2;
-        
-        System.out.println(numero);
+        int numero1 = 2;
+        int numero2 = 2;
+
+        int soma = numero1 + numero2;
+
+        if(soma == 4){
+            System.out.println("verdadeiro");
+        }
+        else{
+            System.out.println("falso");
+        }
+    
+            System.out.println("Fim");
     }
 }
