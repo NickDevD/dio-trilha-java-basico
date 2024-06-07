6edb87e8bd26483c1b663100cc3fc0a4dfbf273a
NickDevD <nick.developerdev@gmail.com>
2024-06-06 21:05:12 -0300

    update 1.3.1

---
+++ tipos-variaveis/src/metodos-parâmetros/metodosLogica.java
@@ -0,0 +1,22 @@
+ 
+//public class metodosLogica {
+
+    //public double somar(int num1, int num2){
+        //return somar(num1, num2);
+    //}
+    
+    //public void imprimir(String texto){
+    // Não retorna valor algum    
+    //}
+//}
+
+//public void gravarCliente(String nome, String cpf, String rg){
+    //este método tem a finalidade de gravar
+    //informações de um cliente, por que não criar
+    //um objeto cliente e passar como parâmetro?
+    //veja abaixo
+//}
+
+//public void gravarCliente(Cliente cliente){
+   
+//}
