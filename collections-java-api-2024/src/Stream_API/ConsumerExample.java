1660b7917924da15deee1303fe434a3f37bc3649
Nicholas Monteiro <nick.developerdev@gmail.com>
2024-08-05 17:20:34 -0300

    atualizações

--- collections-java-api-2024/src/Stream_API/ConsumerExample.java
+++ collections-java-api-2024/src/Stream_API/ConsumerExample.java
@@ -1,2 +1,32 @@
-package Stream_API;public class ConsumerExample {
+package Stream_API;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/*
+    * Representa uma operação que aceita um argumento do tipo T e não retorna nenhum resultado
+    * É utilizada pricipalmente para realizar ações, ou efeitos colaterais no elementos do Stream sem
+    * modificar, ou retornar um valor.
+ */
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+
+        // Criar uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+
+        // Usar o Consumer com expressão lambda para imprimir números pares
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero %2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        // Usar o Consumer para imprimir números pares no Stream
+        numeros.stream().forEach(imprimirNumeroPar);
+    }
+
 }
